00855ff33b456ffd69e6eb116fb1dbbd2408723e
sbowen88 <sbowey@gmail.com>
2019-02-26 10:09:50 -0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import About from '@/components/About'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }, 
+    {
+      path: '/coins/:id',
+      name: 'Coins',
+      component: Coins
+    }
+  ]
+})
